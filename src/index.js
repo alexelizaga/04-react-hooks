8c663d547fe7449db4baa4c00706848626b3af2a
alexelizaga <alexelizaga@icloud.com>
2022-05-05 21:31:30 +0200

    139

--- src/index.js
+++ src/index.js
@@ -14,9 +14,10 @@ import ReactDOM from 'react-dom/client';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TodoApp />
+  <MainApp />
 );
