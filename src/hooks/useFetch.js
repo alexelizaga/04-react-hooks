a20dca18369c034f0860ba94c18feb4c6f88e808
alexelizaga <alexelizaga@icloud.com>
2022-05-05 16:17:07 +0200

    116

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,23 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const useFetch = ( url ) => {
+  const isMounted = useRef(true);
   const [state, setState] = useState({
     data: null,
     loading: true,
     error: null
-  })
+  });
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    }
+  }, [])
+  
 
   useEffect(() => {
     setState({ data: null, loading: true, error: null })
     fetch(url)
       .then( resp => resp.json())
       .then( data => {
-        setState({
-          ...state,
+
+        isMounted.current && setState({
           data,
           loading: false,
           error: null,
         });
+        
       });
   }, [url])
   
