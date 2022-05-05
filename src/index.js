e81c56b381b710933234669daae0eedc009295b1
alexelizaga <alexelizaga@icloud.com>
2022-05-05 18:08:18 +0200

    121

--- src/index.js
+++ src/index.js
@@ -12,10 +12,11 @@ import ReactDOM from 'react-dom/client';
 // import { Layout } from './components/05-layouteEffect/Layout';
 // import { Memorize } from './components/06-memos/Memorize';
 // import { MemoHook } from './components/06-memos/MemoHook';
-import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <CallbackHook />
+  <Padre />
 );
 
